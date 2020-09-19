2a220d51db9cf61a95c22b78a8f34150e466ceef
Zeeyon <zeeyon.c@gmail.com>
2020-09-19 23:50:14 +0900

    lifetime of component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
     state = {
         count: 0,
     };
@@ -17,7 +22,20 @@ class App extends React.Component {
         }));
     };
 
+    componentDidMount() {
+        console.log('component rendered');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('updated');
+    }
+
+    componentWillUnmount() {
+        console.log('goodbye');
+    }
+
     render() {
+        console.log('render');
         return (
             <div>
                 <h1>The number is: {this.state.count}</h1>
