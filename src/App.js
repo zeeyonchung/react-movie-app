ce3f234939544a228f989b23291e4af81dc49809
Zeeyon <zeeyon.c@gmail.com>
2020-10-19 01:48:26 +0900

    라우터

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-    return <Home />;
+    return (
+        <HashRouter>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "Not a shred of evidence exists that life is serious."
+            </span>
+            <span>- Brendan Gill</span>
+        </div>
+    );
+}
+
+export default About;
