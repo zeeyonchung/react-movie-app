3f6534de1d61422524029416791a6fc81f46e031
Zeeyon <zeeyon.c@gmail.com>
2020-10-19 18:20:52 +0900

    네비게이션

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from "./components/Navigation";
+import Detail from "./components/Detail";
 
 function App() {
     return (
         <HashRouter>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/movie-detail" component={Detail} />
         </HashRouter>
     );
 }
